c99462aa865e30f31eb9ac81527277eb50088610
Łukasz <Lukasz19281@gmail.com>
2018-01-11 10:52:40 +0100

    Deletions

--- partial/python_opencv/IncorrectArgumentsException.py
+++
@@ -1,5 +0,0 @@
-class IncorrectArgumentsException(Exception):
-    def __init__(self, message):
-        self.message = message
-    def __str__(self):
-        return self.message
--- times/cpp_opencv/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <iostream>
-#include <ctime>
-
-void timespec_diff(struct timespec*, struct timespec*, struct timespec*);
-
-#endif
